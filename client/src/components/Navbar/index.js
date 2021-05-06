4f1d3a64557a8bb7bdf5c97c1475399d066525e2
avaarm <avaarm95@gmail.com>
2021-05-06 12:51:21 -0700

    navbar styling

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,44 +6,82 @@ import { Link, img } from "react-router-dom";
 // import AccountDropdown from "./AccountDropdown"
 import navak from "./navak.png";
 
-
-
 const style = {
   navbar: {
-      backgroundColor: "transparent"
+    backgroundColor: "transparent",
   },
 };
 
 function Navbar(props) {
   return (
-    <div>
-        <nav className="uk-navbar-container uk-navbar navbar" style={style.navbar}>
-      <Link to="/" className="nav-item" style={{ color: props.color }}><img src={navak} width="100" height="100" alt=" logo"></img></Link>
- <div className="uk-navbar-right">
-                <ul className="uk-navbar-nav uk-visible@m">
-                    {/* <li><Link to="/" className="nav-item" style={{ color: props.color }}>Home</Link></li> */}
-                    <li><Link to="/" className="nav-item" style={{ color: props.color }}>About Us </Link></li>
-                    <li><Link to="/" className="nav-item" style={{ color: props.color }}>Our Vision</Link></li>
-                    <li><Link to="/" className="nav-item" style={{ color: props.color }}>Apply </Link></li>
-                    <li><Link to="/" className="nav-item" style={{ color: props.color }}>Join Us</Link></li>
-                    <li><Link to="/" className="nav-item" style={{ color: props.color }}>Contact</Link></li>
-                </ul>
-                <a className="uk-hidden@m" href="#menu-canvas" uk-toggle="target: #menu-canvas"><span className="menu-icon"
-                    uk-icon="icon: menu; ratio: 1.5"></span></a>
-            </div>
-            {
-                props.accountBtn !== "none" ? (
-                    <div className="uk-navbar-right uk-visible@m">
-                        <button className="uk-button uk-button-default my-account-btn nav-item" type="button" style={{ color: props.color }}>My Account<span className="user-icon" uk-icon="chevron-down"></span></button>
-                        {/* <AccountDropdown /> */}
-                    </div>
-                ) : (
-                        ""
-                    )
-            }
-        </nav>
+    <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
+      <nav
+        className="uk-navbar-container uk-navbar navbar"
+        style={style.navbar}
+      >
+        <Link to="/" className="nav-item" style={{ color: props.color }}>
+          <img src={navak} width="100" height="100" alt=" logo"></img>
+        </Link>
+        <div className="uk-navbar-right">
+          <ul className="uk-navbar-nav uk-visible@m">
+            {/* <li><Link to="/" className="nav-item" style={{ color: props.color }}>Home</Link></li> */}
+            <li>
+              <Link to="/" className="nav-item" style={{ color: props.color }}>
+                Home{" "}
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="nav-item" style={{ color: props.color }}>
+                About Us{" "}
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="nav-item" style={{ color: props.color }}>
+                Our Vision
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="nav-item" style={{ color: props.color }}>
+                Apply{" "}
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="nav-item" style={{ color: props.color }}>
+                Join Us
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="nav-item" style={{ color: props.color }}>
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="nav-item" style={{ color: props.color }}>
+                Eng/Arm
+              </Link>
+            </li>
+          </ul>
+          <a
+            className="uk-hidden@m"
+            href="#menu-canvas"
+            uk-toggle="target: #menu-canvas"
+          >
+            <span className="menu-icon" uk-icon="icon: menu; ratio: 1.5"></span>
+          </a>
+        </div>
+        {
+          // props.accountBtn !== "none" ? (
+          //     <div className="uk-navbar-right uk-visible@m">
+          //         <button className="uk-button uk-button-default my-account-btn nav-item" type="button" style={{ color: props.color }}>My Account<span className="user-icon" uk-icon="chevron-down"></span></button>
+          //         {/* <AccountDropdown /> */}
+          //     </div>
+          // ) : (
+          //         ""
+          //     )
+        }
+      </nav>
     </div>
-)
+  );
 }
 
 //export default is part of the ES6 module system
